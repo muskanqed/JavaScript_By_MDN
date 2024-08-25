8af1a3bf7a27564354ccdd7a2473a5af4a964f55
muskanqed <muskanqed@gmail.com>
2024-08-25 15:06:20 +0530

    Variable declaration in JS

---
+++ Basics_Of_JavaScript/Variable_Declaration.js
@@ -0,0 +1,21 @@
+// var a = 'muskan';
+// var a = 'badeghar';
+// var b = 'harry';
+let b = "john";
+// this cannot be done that is const harry
+// const is always initialized
+const c = "asiya";
+
+// Var is not block scoped
+// {
+//     var a = 'kabir';
+//     console.log(a);
+// }
+
+{
+  let b = "muskan";
+  console.log(b);
+}
+
+console.log(b);
+console.log(c);
