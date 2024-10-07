148edc76dc9403877daf27e4909027395a7d9d26
muskanqed <muskanqed@gmail.com>
2024-10-07 23:45:17 +0530

    Continuing my learning with Hitesh Choudhary teaching

---
+++ ChaiAurCode/Basics_Of_JS/01_Variable_Declaration.js
@@ -0,0 +1,17 @@
+const accountId = "1119299";
+let accountEmail = "muskan@gmail.com";
+var accountPassword = "123";
+accountCity = "Jaipur";
+
+// accountId = "123" // Not allowed because const and const needs to be initialized
+
+accountEmail = "mb@gmail.com";
+accountPassword = "145"
+accountCity = "Banglore";
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity]);
+
+// Never use var as it introduces bugs in code and it is not scope oriented
+// var is not used any more but its still present in old code 
+
