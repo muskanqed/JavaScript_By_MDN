44dac993666845d248ed4d0819b9b41985cf6ff0
muskanqed <muskanqed@gmail.com>
2024-11-10 13:47:01 +0530

    Type Conversion Operations in JS

---
+++ ChaiAurCode/Basics_Of_JS/03_TypeConversion_Operations.js
@@ -0,0 +1,27 @@
+let score = "33ssss";
+
+// we dont know whether we will get a number from frontend end or string
+// but we need a number, here comes type conversion
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(typeof score);
+console.log(valueInNumber); //NaN that is not a number
+
+// if let score = null // output will be 0 which is incorrect
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; 
+
+let isLoggedIn = "1"; // if empty string then false, 0 => false and if string then true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
