34ea77f676b47c35464d2eff60f5db30b4097c2c
muskanqed <muskanqed@gmail.com>
2024-11-11 20:22:12 +0530

    Comparisions in JS

---
+++ ChaiAurCode/Basics_Of_JS/04_Comparision.js
@@ -0,0 +1,25 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+
+console.log("2" == 2); //true
+console.log("2" === 2); //false
+
+console.log(null > 0); // false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+// Comparision converts null to 0 but equality check does not convert null to 0
+// in case of undefined everthing is false
+
+console.log(NaN == 0); //false
+console.log(NaN === 0); //false
+console.log(NaN > 0); //false
+
+
+
+
+
+
+
+
