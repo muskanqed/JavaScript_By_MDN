a108b74c8a538d8cc84238cc806b9d6f74ad5c71
muskanqed <muskanqed@gmail.com>
2024-11-12 13:08:13 +0530

    Datatype Summary

---
+++ ChaiAurCode/Basics_Of_JS/DataTypes_Summary.js
@@ -0,0 +1,44 @@
+// Primitive Data types
+// 1.string
+// 2.Number
+// 3.Boolean
+// 4.undefined
+// 5.null
+// 6.Symbol
+// 7.BigInt
+
+// Reference Data types(Non-Primitive)
+// 1.Arrays
+// 2.Objects
+// 3.Funtions
+
+const isLoggedIn = true;
+const outSideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+const bigNumber = 33685785776575757858758757558587587n;
+
+console.log(bigNumber);
+
+function myFunction(){
+
+}
+
+const myObj ={
+    name:"Muskan",
+    age:20,
+}
+
+// Null typeof will be object
+
+// Function typeof will be function object
+
+// Most of non-primitive datatypes are objects
+
+// Array return type is also object
+
